49e694ad84cabcf5c94998949d4963b96588eba2
hamberan <gnanasekaranhamberan123@gmail.com>
2025-06-24 00:08:28 -0700

    Add files via upload

---
+++ UMS/Repositories/DatabaseManager.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    namespace UMS.Repositories
+    {
+    public static class DatabaseManager
+    {
+        private static string dbFile = "unicomtic.db";
+        private static string connectionString = $"Data Source={dbFile};Version=3;";
+
+        public static void InitializeDatabase()
+        {
+            if (!File.Exists(dbFile))
+            {
+                SQLiteConnection.CreateFile(dbFile);
+            }
+
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                TableCreator.CreateTables(connection);
+            }
+        }
+
+        
+        public static SQLiteConnection GetConnection()
+        {
+            return new SQLiteConnection(connectionString);
+        }
+    }
+}
+
+
+          
